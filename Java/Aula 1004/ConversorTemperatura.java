12c13023c2d9a34080b13975b89faa94c5f8dcbd
muhcrz <cruzmurilohenrique@gmail.com>
2025-04-24 11:14:19 -0300

    commit murilo

---
+++ Java/Aula 1004/ConversorTemperatura.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class  ConversorTemperatura {
+      public static void main(String[] args) {
+        // Declaração de variáveis
+        double cel, fah;
+
+        // Criação e instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        //Apresentação
+        System.out.println("\n\t\t\t -- Conversor de Temperatura --\n");
+      }
+    }
