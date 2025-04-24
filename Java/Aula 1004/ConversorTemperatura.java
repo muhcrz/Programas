34ff329da67e879806477299bd37c56df7f69ca3
henrique4321 <hfranca3214@gmail.com>
2025-04-24 13:48:51 -0300

    commit henrique

--- Java/Aula 1004/ConversorTemperatura.java
+++ Java/Aula 1004/ConversorTemperatura.java
@@ -1,26 +1,46 @@
 import java.util.Scanner;
 
-public class  ConversorTemperatura {
-      public static void main(String[] args) {
+public class ConversorTemperatura {
+    public static void main(String[] args) {
         // Declaração de variáveis
         double cel, fah;
+        int op;
 
         // Criação e instância do objeto de entrada
         Scanner entrada = new Scanner(System.in);
 
-        //Apresentação
-        System.out.println("\n\t\t\t -- Conversor de Temperatura --\n");
-       
-        //Entradas
-        System.out.print("Informe a temperatura em °C: ");
-        cel = entrada.nextDouble();
+        // Apresentação
+            System.out.println("\n\t\t\t -- Conversor de Temperatura --\n");
 
-        //Processamento
-        fah = cel * 9 / 5 + 32;
+            // Menu
+            System.out.println("1 - De Celsius para Fahrenheit");
+            System.out.println("2 - De Fahrenheit para Celsius");
 
-        //Saída
-        System.out.println("\n" + cel + "°C = " + fah + " °F\n");
+            // Opção do usuário
+            System.out.print("Opção: ");
+            op = entrada.nextInt();
 
+            if (op == 1) {
+                // Entrada
+                System.out.print("Informe a temperatura em °C: ");
+                cel = entrada.nextDouble();
 
-      }
+                // Processamento
+                fah = cel * 9 / 5 + 32;
+
+                // Saída
+                System.out.println("\n" + cel + "°C = " + fah + "°F\n");
+            } else if (op == 2) {
+                // Entrada
+                System.out.print("Informe a temperatura em °F: ");
+                fah = entrada.nextDouble();
+
+                // Processamento
+                cel = (fah - 32) * 5 / 9;
+
+                // Saída
+                System.out.println("\n" + fah + "°F = " + cel + "°C\n");
+            }
+              
     }
+  }
