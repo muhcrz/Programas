5ec9c63e6884306ead9fcf83f8c87485aec21c54
pietra <almeidapietra545@gmail.com>
2025-04-24 11:34:38 -0300

    commit pietra

--- Java/Aula 1004/ConversorTemperatura.java
+++ Java/Aula 1004/ConversorTemperatura.java
@@ -10,5 +10,17 @@ public class  ConversorTemperatura {
 
         //Apresentação
         System.out.println("\n\t\t\t -- Conversor de Temperatura --\n");
+       
+        //Entradas
+        System.out.print("Informe a temperatura em °C: ");
+        cel = entrada.nextDouble();
+
+        //Processamento
+        fah = cel * 9 / 5 + 32;
+
+        //Saída
+        System.out.println("\n" + cel + "°C = " + fah + " °F\n");
+
+
       }
     }
